eb9e143ccb95142874bda589b784ffb02d7f4779
LLLDYR <dingdingyr@qq.com>
2021-03-16 23:44:37 +0800

    add

--- src/test/java/com/examples/Hello.java
+++ src/test/java/com/examples/Hello.java
@@ -4,4 +4,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("hello!");
     }
+    public static int add(int a,int b){
+        return a + b;
+    }
 }
